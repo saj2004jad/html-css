e46ef5afbde62b7411af6c8222d56fd7f6524c33
sajjad saleh <sajjad.saleh2004@gmail.com>
2023-11-26 00:22:01 +0330

    part #10

--- noname/assets/js/user.js
+++ noname/assets/js/user.js
@@ -1,4 +1,157 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.collapsible');
-    var instances = M.Collapsible.init(elems, {});
+document.addEventListener('DOMContentLoaded', function () {
+  var elems = document.querySelectorAll('.collapsible');
+  var instances = M.Collapsible.init(elems, {});
+
+  // const addUserbutton = document.getElementById("add_user_button")
+  // console.log('______________________');
+  // console.log(addUserbutton);
+  // addUserbutton.addEventListener('click', () => {
+  //   document.querySelector(".add_user_modal").classList.add("show")
+  //   document.querySelector(".add_user_modal_back").classList.remove("dis-none")
+  // })
+
+  // document.querySelector(".add_user_modal_back")
+  //   .addEventListener("click", (e) => {
+  //     document.querySelector(".add_user_modal").classList.remove("show")
+  //     e.target.classList.add("dis-none")
+  //   })
+
+
+
+
+
+
+    // setTimeout(function () {
+    //   console.log("start...");
+    //   fetch('https://jsonplaceholder.typicode.com/users')
+    
+    //     .then((response) => response.json())
+    //     .then((json) => {
+    //       console.log("end...");
+    //       console.log(json)
+    
+    //       let html = ""
+    
+    //       for (const user of json) {
+    //         html = html +
+    //           `<tr>
+    //         <td>${user.id}</td>
+    //         <td>${user.name}</td>
+    //         <td>${user.username}</td>
+    //         <td>${user.email}</td>
+    //         <td>${user.address.city}</td>
+    //         <td>
+    //           <i class="material-icons green-text m-l-5 m-r-5 hov-pointer">edit</i>
+    //           <i class="material-icons red-text m-l-5 m-r-5 hov-pointer">delete_forever</i>
+    //         </td>
+    //       </tr>`
+    //       }
+    //       document.querySelector(".desktop_table_body").innerHTML = html
+    //     });
+    // }, 5000)
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   });
